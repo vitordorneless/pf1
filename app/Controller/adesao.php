9d80011b34f7c395e03a4e819cb4b7c44080d76b
vitordorneless <vitordorneles@hotmail.com>
2017-09-09 11:24:34 -0300

    correcao no controller
    correção para os dependentes, porque acima de 8 ele estava duplicando o
    último

--- app/Controller/adesao.php
+++ app/Controller/adesao.php
@@ -37,7 +37,30 @@ $confirm = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura
 //dependentes
 $dep_nome = filter_input(INPUT_GET, 'dep_nome', FILTER_SANITIZE_STRING);
 $dep_cpf = filter_input(INPUT_GET, 'dep_cpf', FILTER_SANITIZE_STRING);
-if (!empty($dep_nome) and !empty($dep_cpf)) {
+$dep_nome1 = filter_input(INPUT_GET, 'dep_nome1', FILTER_SANITIZE_STRING);
+$dep_cpf1 = filter_input(INPUT_GET, 'dep_cpf1', FILTER_SANITIZE_STRING);
+$dep_nome2 = filter_input(INPUT_GET, 'dep_nome2', FILTER_SANITIZE_STRING);
+$dep_cpf2 = filter_input(INPUT_GET, 'dep_cpf2', FILTER_SANITIZE_STRING);
+$dep_nome3 = filter_input(INPUT_GET, 'dep_nome3', FILTER_SANITIZE_STRING);
+$dep_cpf3 = filter_input(INPUT_GET, 'dep_cpf3', FILTER_SANITIZE_STRING);
+$dep_nome4 = filter_input(INPUT_GET, 'dep_nome4', FILTER_SANITIZE_STRING);
+$dep_cpf4 = filter_input(INPUT_GET, 'dep_cpf4', FILTER_SANITIZE_STRING);
+$dep_nome5 = filter_input(INPUT_GET, 'dep_nome5', FILTER_SANITIZE_STRING);
+$dep_cpf5 = filter_input(INPUT_GET, 'dep_cpf5', FILTER_SANITIZE_STRING);
+$dep_nome6 = filter_input(INPUT_GET, 'dep_nome6', FILTER_SANITIZE_STRING);
+$dep_cpf6 = filter_input(INPUT_GET, 'dep_cpf6', FILTER_SANITIZE_STRING);
+$dep_nome7 = filter_input(INPUT_GET, 'dep_nome7', FILTER_SANITIZE_STRING);
+$dep_cpf7 = filter_input(INPUT_GET, 'dep_cpf7', FILTER_SANITIZE_STRING);
+$dep_nome8 = filter_input(INPUT_GET, 'dep_nome8', FILTER_SANITIZE_STRING);
+$dep_cpf8 = filter_input(INPUT_GET, 'dep_cpf8', FILTER_SANITIZE_STRING);
+$dep_nome9 = filter_input(INPUT_GET, 'dep_nome9', FILTER_SANITIZE_STRING);
+$dep_cpf9 = filter_input(INPUT_GET, 'dep_cpf9', FILTER_SANITIZE_STRING);
+$dep_nome10 = filter_input(INPUT_GET, 'dep_nome10', FILTER_SANITIZE_STRING);
+$dep_cpf10 = filter_input(INPUT_GET, 'dep_cpf10', FILTER_SANITIZE_STRING);
+$dep_nome11 = filter_input(INPUT_GET, 'dep_nome11', FILTER_SANITIZE_STRING);
+$dep_cpf11 = filter_input(INPUT_GET, 'dep_cpf11', FILTER_SANITIZE_STRING);
+
+if (($dep_nome !== 'no') and ( $dep_cpf !== 'no')) {
     $dep_tel = filter_input(INPUT_GET, 'dep_tel', FILTER_SANITIZE_STRING);
     $dep_nome_mae = filter_input(INPUT_GET, 'dep_nome_mae', FILTER_SANITIZE_STRING);
     $dep_email = filter_input(INPUT_GET, 'dep_email', FILTER_SANITIZE_STRING);
@@ -46,90 +69,119 @@ if (!empty($dep_nome) and !empty($dep_cpf)) {
     $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome, $dep_email, $dep_nome_mae, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel, $dep_sexo, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco);
     $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
 }
-/*
 
+if (($dep_nome1 !== 'no') and ( $dep_cpf1 !== 'no')) {
+    $dep_tel1 = filter_input(INPUT_GET, 'dep_tel1', FILTER_SANITIZE_STRING);
+    $dep_nome_mae1 = filter_input(INPUT_GET, 'dep_nome_mae1', FILTER_SANITIZE_STRING);
+    $dep_email1 = filter_input(INPUT_GET, 'dep_email1', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco1 = filter_input(INPUT_GET, 'dep_grau_parentesco1', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo1 = filter_input(INPUT_GET, 'dep_sexo1', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome1, $dep_email1, $dep_nome_mae1, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel1, $dep_sexo1, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco1);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
 
+if (($dep_nome2 !== 'no') and ( $dep_cpf2 !== 'no')) {
+    $dep_tel2 = filter_input(INPUT_GET, 'dep_tel2', FILTER_SANITIZE_STRING);
+    $dep_nome_mae2 = filter_input(INPUT_GET, 'dep_nome_mae2', FILTER_SANITIZE_STRING);
+    $dep_email2 = filter_input(INPUT_GET, 'dep_email2', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco2 = filter_input(INPUT_GET, 'dep_grau_parentesco2', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo2 = filter_input(INPUT_GET, 'dep_sexo2', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome2, $dep_email2, $dep_nome_mae2, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel2, $dep_sexo2, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco2);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
 
-  dep_nome1
-  dep_tel1
-  dep_cpf1
-  dep_nome_mae1
-  dep_email1
-  dep_grau_parentesco1
-  dep_sexo1
-  dep_nome2
-  dep_tel2
-  dep_cpf2
-  dep_nome_mae2
-  dep_email2
-  dep_grau_parentesco2
-  dep_sexo2
-  dep_nome3
-  dep_tel3
-  dep_cpf3
-  dep_nome_mae3
-  dep_email3
-  dep_grau_parentesco3
-  dep_sexo3
-  dep_nome4
-  dep_tel4
-  dep_cpf4
-  dep_nome_mae4
-  dep_email4
-  dep_grau_parentesco4
-  dep_sexo4
-  dep_nome5
-  dep_tel5
-  dep_cpf5
-  dep_nome_mae5
-  dep_email5
-  dep_grau_parentesco5
-  dep_sexo5
-  dep_nome6
-  dep_tel6
-  dep_cpf6
-  dep_nome_mae6
-  dep_email6
-  dep_grau_parentesco6
-  dep_sexo6
-  dep_nome7
-  dep_tel7
-  dep_cpf7
-  dep_nome_mae7
-  dep_email7
-  dep_grau_parentesco7
-  dep_sexo7
-  dep_nome8
-  dep_tel8
-  dep_cpf8
-  dep_nome_mae8
-  dep_email8
-  dep_grau_parentesco8
-  dep_sexo8
-  dep_nome9
-  dep_tel9
-  dep_cpf9
-  dep_nome_mae9
-  dep_email9
-  dep_grau_parentesco9
-  dep_sexo9
-  dep_nome10
-  dep_tel10
-  dep_cpf10
-  dep_nome_mae10
-  dep_email10
-  dep_grau_parentesco10
-  dep_sexo10
-  dep_nome11
-  dep_tel11
-  dep_cpf11
-  dep_nome_mae11
-  dep_email11
-  dep_grau_parentesco11
-  dep_sexo11 */
+if (($dep_nome3 !== 'no') and ( $dep_cpf3 !== 'no')) {
+    $dep_tel3 = filter_input(INPUT_GET, 'dep_tel3', FILTER_SANITIZE_STRING);
+    $dep_nome_mae3 = filter_input(INPUT_GET, 'dep_nome_mae3', FILTER_SANITIZE_STRING);
+    $dep_email3 = filter_input(INPUT_GET, 'dep_email3', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco3 = filter_input(INPUT_GET, 'dep_grau_parentesco3', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo3 = filter_input(INPUT_GET, 'dep_sexo3', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome3, $dep_email3, $dep_nome_mae3, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel3, $dep_sexo3, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco3);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
+
+if (($dep_nome4 !== 'no') and ( $dep_cpf4 !== 'no')) {
+    $dep_tel4 = filter_input(INPUT_GET, 'dep_tel4', FILTER_SANITIZE_STRING);
+    $dep_nome_mae4 = filter_input(INPUT_GET, 'dep_nome_mae4', FILTER_SANITIZE_STRING);
+    $dep_email4 = filter_input(INPUT_GET, 'dep_email4', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco4 = filter_input(INPUT_GET, 'dep_grau_parentesco4', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo4 = filter_input(INPUT_GET, 'dep_sexo4', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome4, $dep_email4, $dep_nome_mae4, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel4, $dep_sexo4, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco4);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
+
+if (($dep_nome5 !== 'no') and ( $dep_cpf5 !== 'no')) {
+    $dep_tel5 = filter_input(INPUT_GET, 'dep_tel5', FILTER_SANITIZE_STRING);
+    $dep_nome_mae5 = filter_input(INPUT_GET, 'dep_nome_mae5', FILTER_SANITIZE_STRING);
+    $dep_email5 = filter_input(INPUT_GET, 'dep_email5', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco5 = filter_input(INPUT_GET, 'dep_grau_parentesco5', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo5 = filter_input(INPUT_GET, 'dep_sexo5', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome5, $dep_email5, $dep_nome_mae5, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel5, $dep_sexo5, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco5);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
+
+if (($dep_nome6 !== 'no') and ( $dep_cpf6 !== 'no')) {
+    $dep_tel6 = filter_input(INPUT_GET, 'dep_tel6', FILTER_SANITIZE_STRING);
+    $dep_nome_mae6 = filter_input(INPUT_GET, 'dep_nome_mae6', FILTER_SANITIZE_STRING);
+    $dep_email6 = filter_input(INPUT_GET, 'dep_email6', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco6 = filter_input(INPUT_GET, 'dep_grau_parentesco6', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo6 = filter_input(INPUT_GET, 'dep_sexo6', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome6, $dep_email6, $dep_nome_mae6, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel6, $dep_sexo6, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco6);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
+
+if (($dep_nome7 !== 'no') and ( $dep_cpf7 !== 'no')) {
+    $dep_tel7 = filter_input(INPUT_GET, 'dep_tel7', FILTER_SANITIZE_STRING);
+    $dep_nome_mae7 = filter_input(INPUT_GET, 'dep_nome_mae7', FILTER_SANITIZE_STRING);
+    $dep_email7 = filter_input(INPUT_GET, 'dep_email7', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco7 = filter_input(INPUT_GET, 'dep_grau_parentesco7', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo7 = filter_input(INPUT_GET, 'dep_sexo7', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome7, $dep_email7, $dep_nome_mae7, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel7, $dep_sexo7, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco7);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
+
+if (($dep_nome8 !== 'no') and ( $dep_cpf8 !== 'no')) {
+    $dep_tel8 = filter_input(INPUT_GET, 'dep_tel8', FILTER_SANITIZE_STRING);
+    $dep_nome_mae8 = filter_input(INPUT_GET, 'dep_nome_mae8', FILTER_SANITIZE_STRING);
+    $dep_email8 = filter_input(INPUT_GET, 'dep_email8', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco8 = filter_input(INPUT_GET, 'dep_grau_parentesco8', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo8 = filter_input(INPUT_GET, 'dep_sexo8', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome8, $dep_email8, $dep_nome_mae8, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel8, $dep_sexo8, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco8);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
+
+if (($dep_nome9 !== 'no') and ( $dep_cpf9 !== 'no')) {
+    $dep_tel9 = filter_input(INPUT_GET, 'dep_tel9', FILTER_SANITIZE_STRING);
+    $dep_nome_mae9 = filter_input(INPUT_GET, 'dep_nome_mae9', FILTER_SANITIZE_STRING);
+    $dep_email9 = filter_input(INPUT_GET, 'dep_email9', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco9 = filter_input(INPUT_GET, 'dep_grau_parentesco9', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo9 = filter_input(INPUT_GET, 'dep_sexo9', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome9, $dep_email9, $dep_nome_mae9, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel9, $dep_sexo9, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco9);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
+
+if (($dep_nome10 !== 'no') and ( $dep_cpf10 !== 'no')) {
+    $dep_tel10 = filter_input(INPUT_GET, 'dep_tel10', FILTER_SANITIZE_STRING);
+    $dep_nome_mae10 = filter_input(INPUT_GET, 'dep_nome_mae10', FILTER_SANITIZE_STRING);
+    $dep_email10 = filter_input(INPUT_GET, 'dep_email10', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco10 = filter_input(INPUT_GET, 'dep_grau_parentesco10', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo10 = filter_input(INPUT_GET, 'dep_sexo10', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome10, $dep_email10, $dep_nome_mae10, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel10, $dep_sexo10, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco10);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
+
+if (($dep_nome11 !== 'no') and ( $dep_cpf11 !== 'no')) {
+    $dep_tel11 = filter_input(INPUT_GET, 'dep_tel11', FILTER_SANITIZE_STRING);
+    $dep_nome_mae11 = filter_input(INPUT_GET, 'dep_nome_mae11', FILTER_SANITIZE_STRING);
+    $dep_email11 = filter_input(INPUT_GET, 'dep_email11', FILTER_SANITIZE_STRING);
+    $dep_grau_parentesco11 = filter_input(INPUT_GET, 'dep_grau_parentesco11', FILTER_SANITIZE_NUMBER_INT);
+    $dep_sexo11 = filter_input(INPUT_GET, 'dep_sexo11', FILTER_SANITIZE_NUMBER_INT);
+    $confirm0 = $adesao->save($adesao->get_hash_de_vinvulo(), $adesao->get_ip_captura(), $adesao->get_titular_dep(), $adesao->get_cpf_titular(), $adesao->get_cpf_dependente(), $dep_nome11, $dep_email11, $dep_nome_mae11, $adesao->get_telefone(), $adesao->get_nascimento(), $dep_tel11, $dep_sexo11, $adesao->get_cep(), $adesao->get_rua(), $adesao->get_bairro(), $adesao->get_numero(), $adesao->get_complemento(), $adesao->get_cidade(), $adesao->get_uf(), $adesao->get_aceite_termos_de_uso(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_hora_entrada_dos_dados(), $adesao->get_localizacao(), $dep_grau_parentesco11);
+    $cont_deps = $confirm0 == TRUE ? ++$cont_deps : $cont_deps;
+}
 
 if ($confirm === TRUE) {
-    echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>Sucesso</div>';
+    echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>Sucesso<br> Dependentes incluídos: ' . $cont_deps . '</div>';
 } else {
     echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>Erro...</div>';
 }
